aa57323b9a03398b4dc6b2e302027e12b82f54f1
Laize Ferraz <laizeferraz@gmail.com>
2023-07-08 20:13:30 +1000

    create home layout for mobile

---
+++ src/components/HomeMobile.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import illustrationImg from "../../public/images/illustration.svg"
+import logoYellow from "../../public/images/logoYellow.svg" 
+import googleImg from "../../public/images/google-icon.svg"
+import enterBlack from "../../public/images/enterBlack.svg"
+import { Button } from "./Button";
+
+export const HomeMobile = () => {
+  return (
+        <div className="flex flex-col justify-center items-center bg-blue-500 p-2 min-h-screen">
+        <Image className="self-center" src={logoYellow} alt="Yellow letmeask logo " />
+        <Image className="max-w-[280px]" src={illustrationImg} alt="Questions and Answers illustration." />
+        <div className="flex flex-col">
+        <Button customStyle="bg-red-500 text-white-100 hover:bg-red-600 hover:text-white-200">
+          <Image className="items-center" src={googleImg} alt="Google logo" />
+          Create your room with Google
+        </Button>
+        <div className="text-white-200 before:grow before:bg-white-200 before:mr-1 before:h-[1px] text-xs my-6 flex items-center before:content-[''] after:content-[''] after:bg-white-200 after:grow after:h-[1px] after:ml-1">or enter in a room</div>
+        <Button customStyle="bg-white-200 text-black-700 hover:bg-red-600 hover:text-white-200">
+          <Image src={enterBlack} alt="enter icon" />Enter in a room
+        </Button>
+        </div>
+      </div>
+  )
+}
