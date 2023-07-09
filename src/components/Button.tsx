01b073ab5f15b6e9e173431b2b04df8f23d2705a
Laize Ferraz <laizeferraz@gmail.com>
2023-07-09 20:28:16 +1000

    update layout and add prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,16 @@
+import { MouseEvent } from 'react'
 interface ButtonProps {
   children: React.ReactNode
   customStyle?: string
   type?: "button" | "submit" | "reset" 
   isDisabled?: boolean
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void
 }
 
-export const Button = ({children, type, customStyle, isDisabled=false}: ButtonProps) => {
+export const Button = ({children, type, customStyle, isDisabled=false, onClick}: ButtonProps) => {
   return (
     <div>
-      <button className={`${customStyle} ${type} ${isDisabled} w-80 rounded-lg px-2 h-[50px] flex items-center justify-center gap-2 `}>{children}</button>
+      <button onClick={onClick} className={`${customStyle} ${type} ${isDisabled} w-full rounded-lg p-2 max-h-[50px] lg:h-[50px] flex items-center justify-center gap-2`}>{children}</button>
     </div>
   )
 }
