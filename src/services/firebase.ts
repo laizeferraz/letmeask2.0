ba5d9b8c0b0d3929e71194b164d5972a03a64843
Laize Ferraz <laizeferraz@gmail.com>
2023-07-09 20:25:20 +1000

    update auth with google provider

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
@@ -14,5 +14,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
-export const auth = getAuth(app)
-export const database = getDatabase(app)
+const auth = getAuth()
+const database = getDatabase()
+const googleProvider = new GoogleAuthProvider()
+
+export {app, auth, database, googleProvider}
