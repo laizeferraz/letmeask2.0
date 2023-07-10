59315ef8585dc4cdf8f6e43fe6b8bed877dbe3ea
Laize Ferraz <laizeferraz@gmail.com>
2023-07-10 22:02:43 +1000

    add like question functionality

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -4,13 +4,12 @@ import { FormEvent, useState } from "react";
 import logoImg from "../../../../public/images/logoBlue.svg";
 import { Button } from "@/components/Button";
 import { useAuth } from "@/hooks/useAuth";
-import { ref, set, push } from "firebase/database";
+import { ref, set, push, remove } from "firebase/database";
 import Image from "next/image";
 import { RoomCode } from "@/components/RoomCode";
 import emptyQuestionsImg from "../../../../public/images/empty-questions.svg";
 import { useParams } from "next/navigation";
 import { database } from "@/services/firebase";
-import likeImg from "../../../../public/images/like.svg";
 import { Question } from "@/components/Question";
 import { useRoom } from "@/hooks/useRoom";
 
@@ -48,6 +47,22 @@ export default function Room () {
     await set(newQuestionRef, question)
 
     setNewQuestion("");
+  }
+
+  async function handleLikeQuestion(
+    questionId: string,
+    likeId: string | undefined
+  ) {
+    if (likeId) {
+      //Remove like
+      const likedRef = await ref(database, `rooms/${roomId}/questions/${questionId}/likes/${likeId}`)
+      remove(likedRef);
+    } else {
+      const likeRef = await ref(database, `rooms/${roomId}/questions/${questionId}/likes`)
+      push(likeRef, {
+        authorId: user?.id,
+      });
+    }
   }
 
   return (
@@ -89,6 +104,13 @@ export default function Room () {
             </Button>
           </div>
         </form>
+        {questions.length === 0 && (
+          <div className="flex flex-col justify-center items-center mt-10">
+            <Image className="mb-10" src={emptyQuestionsImg} alt="Astronauts" />
+            <span className="font-secondary font-bold text-black-700">No questions yet</span>
+            <span className="text-sm text-gray-500">Login and be the first asking questions!</span>
+          </div>
+        )}
         <div className="mt-4">
           {questions.map((question) => {
             return (
@@ -101,29 +123,25 @@ export default function Room () {
               >
                 {!question.isAnswered && (
                   <button
-                    className={``}
+                    className={`flex items-center gap-1 text-gray-500 hover:text-blue-500 transition-colors duration-200`}
                     type="button"
                     aria-label="Give a like"
+                    onClick={() =>
+                      handleLikeQuestion(question.id, question.likeId)
+                    }
                   >
-                    <span className="text-sm lg:text-base text-black-700">{`0`}</span>
-
-                    <Image
-                      className="h-4 w-4 lg:h-6 lg:w-6"
-                      src={likeImg}
-                      alt="Like"/>
+                    {question.likeCount > 0 && (
+                      <span className="text-sm lg:text-base text-black-700">{question.likeCount}</span>
+                    )}
+                    <svg className="h-[20px] w-[20px] -mt-2 lg:h-[24px] lg:w-[24px]" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z" stroke={`${question.likeId ? "stroke-blue-500": "#737380"}`} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
                   </button>
                 )}
               </Question>
             );
           })}
         </div>
-        {questions.length === 0 && (
-          <div className="flex flex-col justify-center items-center mt-10">
-            <Image className="mb-10" src={emptyQuestionsImg} alt="Astronauts" />
-            <span className="font-secondary font-bold text-black-700">No questions yet</span>
-            <span className="text-sm text-gray-500">Login and be the first asking questions!</span>
-          </div>
-        )}
       </main>
     </div>
   );
