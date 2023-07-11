c591003cf689b2894359ab89af7880225bd406eb
Laize Ferraz <laizeferraz@gmail.com>
2023-07-11 21:29:57 +1000

    add login and highlight question

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -16,10 +16,17 @@ import { useRoom } from "@/hooks/useRoom";
 
 export default function Room () {
   const params = useParams();
-  const { user } = useAuth();
+  const { user, signInWithGoogle } = useAuth();
   const [newQuestion, setNewQuestion] = useState("");
   const roomId = params.id;
   const { questions, title } = useRoom(roomId);
+
+
+  async function handleSignIn () {
+    if (!user) {
+      await signInWithGoogle()
+    }
+  }
 
   async function handleSendQuestion(e: FormEvent) {
     e.preventDefault();
@@ -38,7 +45,7 @@ export default function Room () {
         name: user.name,
         avatar: user.avatar,
       },
-      isHighligthed: false,
+      isHighlighted: false,
       isAnswered: false,
     };
 
@@ -96,7 +103,8 @@ export default function Room () {
               </div>
             ) : (
               <span className="font-medium text-gray-500 text-sm">
-                To ask a question, <button className="bg-transparent border-0 text-blue-500 underline text-base cursor-pointer font-medium">please login</button>
+                To ask a question, <button className="bg-transparent border-0 text-blue-500 underline text-base cursor-pointer font-medium"
+                onClick={handleSignIn}>please login.</button>
               </span>
             )}
             <Button customStyle="px-2 py-1 lg:p-2 bg-blue-500 text-white-100 hover:bg-blue-600 hover:text-white-200 mt-4" type="submit" isDisabled={!user}>
@@ -104,13 +112,13 @@ export default function Room () {
             </Button>
           </div>
         </form>
-        {questions.length === 0 && (
+        {questions.length === 0 ? (
           <div className="flex flex-col justify-center items-center mt-10">
             <Image className="mb-10" src={emptyQuestionsImg} alt="Astronauts" />
             <span className="font-secondary font-bold text-black-700">No questions yet</span>
             <span className="text-sm text-gray-500">Login and be the first asking questions!</span>
           </div>
-        )}
+        ): (
         <div className="mt-4">
           {questions.map((question) => {
             return (
@@ -133,7 +141,7 @@ export default function Room () {
                     {question.likeCount > 0 && (
                       <span className="text-sm lg:text-base text-black-700">{question.likeCount}</span>
                     )}
-                    <svg className="h-[20px] w-[20px] -mt-2 lg:h-[24px] lg:w-[24px]" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className={`${question.likeId ? "stroke-blue-500" : "stroke-black-700"} h-[20px] w-[20px] -mt-2 lg:h-[24px] lg:w-[24px]`} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z" stroke={`${question.likeId ? "stroke-blue-500": "#737380"}`} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                   </button>
@@ -142,6 +150,7 @@ export default function Room () {
             );
           })}
         </div>
+        )}
       </main>
     </div>
   );
