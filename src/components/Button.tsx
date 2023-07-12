0d13157d6aebb24579823b1bcf2621f66167d3c9
Laize Ferraz <laizeferraz@gmail.com>
2023-07-12 21:30:06 +1000

    add all button html attributes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,13 @@
-import { MouseEvent } from 'react'
-interface ButtonProps {
+import { ButtonHTMLAttributes } from 'react'
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode
   customStyle?: string
-  type?: "button" | "submit" | "reset" 
-  isDisabled?: boolean
-  onClick?: (e: MouseEvent<HTMLButtonElement>) => void
 }
 
-export const Button = ({children, type, customStyle, isDisabled=false, onClick}: ButtonProps) => {
+export const Button = ({children, customStyle, ...props}: ButtonProps) => {
   return (
     <div>
-      <button onClick={onClick} className={`${customStyle} ${type} ${isDisabled} w-full rounded-lg p-2 max-h-[50px] lg:h-[50px] flex items-center justify-center gap-2`}>{children}</button>
+      <button {...props} className={`${customStyle} w-full rounded-lg p-2 max-h-[50px] lg:h-[50px] flex items-center justify-center gap-2`}>{children}</button>
     </div>
   )
 }
