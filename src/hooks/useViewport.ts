c534f69cffbb59b754b1690f0294bc5d3b3f2b73
Laize Ferraz <laizeferraz@gmail.com>
2023-07-15 18:46:52 +1000

    fix display mobile & then desktop in large screen

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react"
 
 export const useViewport = () => {
-  const [width, setWidth] = useState(0)
+  const [width, setWidth] = useState(window.innerWidth)
 
   useEffect(() => {
     setWidth(window.innerWidth)
 
     window.addEventListener("resize", () => setWidth(window.innerWidth))
     return () =>
-      window.removeEventListener("resize", () => setWidth(window.innerWidth))
+    window.removeEventListener("resize", () => setWidth(window.innerWidth))
   }, [])
 
   return { width }
