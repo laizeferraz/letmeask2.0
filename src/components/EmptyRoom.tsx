52d0dfdb3b51cc0b7e6058f25aa3e17cf2a62b27
Laize Ferraz <laizeferraz@gmail.com>
2023-07-22 20:38:34 +1000

    fix typo

--- src/components/EmptyRoom.tsx
+++ src/components/EmptyRoom.tsx
@@ -8,7 +8,7 @@ interface EmptyRoomProps {
 export const EmptyRoom = ({text}: EmptyRoomProps) => {
   return(
     <div className="flex flex-col justify-center items-center mt-10">
-            <Image className="mb-10" src={noQuestionsYet} alt="Astronauts" />
+            <Image className="mb-10" src={noQuestionsYet} alt="Astronauts" priority />
             <span className="font-secondary font-bold text-black-700">No questions yet...</span>
             <span className="text-sm text-gray-500">{text}</span>
           </div>
